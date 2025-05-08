511a6ab462945ea484017db0b50c87e38f7e8803
deepakcode21@gmail.com <deepakcode21@gmail.com>
2025-05-08 20:24:10 +0530

    fix bug

--- server/app/main.py
+++ server/app/main.py
@@ -21,7 +21,7 @@ app = FastAPI()
 # CORS settings
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=["https://auto-ml-studio-phi.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
